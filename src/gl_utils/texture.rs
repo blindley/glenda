b5f4b9b667dc039ed208a08e9792657ea2b5f61a
Benjamin Lindley <benjameslindley@gmail.com>
2025-05-07 16:55:12 -0500

    finish texture filter and wrap

--- src/gl_utils/texture.rs
+++ src/gl_utils/texture.rs
@@ -21,7 +21,13 @@ pub fn create_texture(format: GLenum, size: (u32, u32), data: &[u8])
             gl::UNSIGNED_BYTE,
             data.as_ptr() as _,
         );
+
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
     }
+
 
     Ok(texture)
 }
